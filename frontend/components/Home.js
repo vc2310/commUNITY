9936723d5bc856cd2784f107d946e11c4a166cd7
Faisal Jaffer <faisalmuh786@gmail.com>
2021-01-18 12:43:26 -0500

    removing unnecessarily tracked files + added mapbox

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -3,6 +3,9 @@ import { View, TextInput, Alert } from "react-native";
 import { Container, Header, Content, Button, Text, H1 } from "native-base";
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import {logout} from "../services/auth/AuthService"
+import MapboxGL from "@react-native-mapbox-gl/maps";
+
+MapboxGL.setAccessToken("pk.eyJ1IjoiZmFpc2FsbXVoNzg2IiwiYSI6ImNrODRucXg2YjBjMnAzbW1yNjZ3ZHNqd3oifQ.-BuJJq_CEkUXCMjeypdSdg");
 
 class Home extends React.Component {
   static navigationOptions = {
@@ -21,17 +24,32 @@ class Home extends React.Component {
   }
   render () {
     return (
-      <Container>
-        <View style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}>
-          <Button danger onPress={()=> {logout().then(this.props.navigation.navigate("Login")).catch((error)=>{console.log(error)})}}>
-            <Text>LogOut</Text>
-          </Button>
-        </View>
-      </Container>
+      // <Container>
+      //   <View style={{
+      //     flex: 1,
+      //     justifyContent: "center",
+      //     alignItems: "center",
+      //   }}>
+      //     <Button danger onPress={()=> {logout().then(this.props.navigation.navigate("Login")).catch((error)=>{console.log(error)})}}>
+      //       <Text>LogOut</Text>
+      //     </Button>
+      //   </View>
+      // </Container>
+      <View style={{flex: 1, height: "100%", width: "100%" }}>
+      <MapboxGL.MapView
+        styleURL={MapboxGL.StyleURL.Street}
+        zoomLevel={16}
+        centerCoordinate={[3.3362400, 6.5790100]}
+        style={{flex: 1}}>
+           <MapboxGL.Camera
+              zoomLevel={16}
+              centerCoordinate={[3.3362400, 6.5790100]}
+              animationMode={'flyTo'}
+              animationDuration={0}
+          	>
+          </MapboxGL.Camera>
+      </MapboxGL.MapView>
+    </View>
     );
   }
 }
