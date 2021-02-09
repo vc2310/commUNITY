a8cb8fbc8d09e40747485f4e9f97e72cac68a023
Vaibhav Chadha <vaibhav@Vaibhavs-MacBook-Pro.local>
2021-02-09 17:58:34 -0500

    adding the website base

---
+++ website/src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
